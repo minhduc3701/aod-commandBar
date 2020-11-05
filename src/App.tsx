6bc78962f137af9318ccecb43991c1db6d193b7b
nmd <minh.duc.3701@gmail.com>
2020-11-05 10:07:47 +0700

    first

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,118 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Toggle } from "aod-dependencies/Toggle";
+import { initializeIcons } from "aod-dependencies/@uifabric/icons";
+import { Wrapper } from "./AppStyle";
+import CustomCommandBar from "aod-dependencies/CommandBar/CustomCommandBar";
+import { ICommandBarItemProps } from "aod-dependencies/CommandBar/CommandBar.types";
+
+import "./App.css";
+
+initializeIcons();
+
+const _items: ICommandBarItemProps[] = [
+  {
+    key: "newItem",
+    text: "New",
+    cacheKey: "myCacheKey", // changing this key will invalidate this item's cache
+    iconProps: { iconName: "Add" },
+    subMenuProps: {
+      items: [
+        {
+          key: "emailMessage",
+          text: "Email message",
+          iconProps: { iconName: "Mail" },
+          // ["data-automation-id"]: "newEmailButton", // optional
+        },
+        {
+          key: "calendarEvent",
+          text: "Calendar event",
+          iconProps: { iconName: "Calendar" },
+        },
+      ],
+    },
+  },
+  {
+    key: "upload",
+    text: "Upload",
+    iconProps: { iconName: "Upload" },
+    href: "https://developer.microsoft.com/en-us/fluentui",
+  },
+  {
+    key: "share",
+    text: "Share",
+    iconProps: { iconName: "Share" },
+    onClick: () => console.log("Share"),
+  },
+  {
+    key: "download",
+    text: "Download",
+    iconProps: { iconName: "Download" },
+    onClick: () => console.log("Download"),
+  },
+  {
+    key: "move",
+    text: "Move to",
+    onClick: () => console.log("Move to"),
+    iconProps: { iconName: "MoveToFolder" },
+  },
+  {
+    key: "copy",
+    text: "Copy to",
+    onClick: () => console.log("Copy to"),
+    iconProps: { iconName: "Copy" },
+  },
+  {
+    key: "rename",
+    text: "Rename",
+    onClick: () => console.log("Rename"),
+    iconProps: { iconName: "Edit" },
+  },
+];
+
+const _farItems: ICommandBarItemProps[] = [
+  {
+    key: "tile",
+    text: "Grid view",
+    // This needs an ariaLabel since it's icon-only
+    ariaLabel: "Grid view",
+    iconOnly: true,
+    iconProps: { iconName: "Tiles" },
+    onClick: () => console.log("Tiles"),
+  },
+  {
+    key: "info",
+    text: "Info",
+    // This needs an ariaLabel since it's icon-only
+    ariaLabel: "Info",
+    iconOnly: true,
+    iconProps: { iconName: "Info" },
+    onClick: () => console.log("Info"),
+  },
+];
 
 function App() {
+  const [darkMode, setDarkMode] = React.useState<string>("");
+  const onChangeMode = () => {
+    if (darkMode === "dark") {
+      setDarkMode("light");
+    }
+    if (darkMode !== "dark") {
+      setDarkMode("dark");
+    }
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Wrapper theme={darkMode}>
+        <div className="toggle-wrapper">
+          <Toggle label="Dark mode" onChange={onChangeMode} />
+        </div>
+        <CustomCommandBar
+          items={_items}
+          farItems={_farItems}
+          ariaLabel="Use left and right arrow keys to navigate between commands"
+          darkMode={darkMode}
+        />
+      </Wrapper>
     </div>
   );
 }
